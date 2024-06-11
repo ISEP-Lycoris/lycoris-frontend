30a59c815f4e5d6f948f8e96b7263c8d8e665948
Mattlpt <matthieu.lpt56@gmail.com>
2024-06-11 22:48:51 +0200

    events displayed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import {Inter} from "next/font/google";
 import {useRouter} from "next/router";
 import {Container} from "@/components/layout/container";
 import {Desktop, Mobile} from "@/components/layout/responsive";
-import {Breadcrumbs} from "@mui/material";
+import {Breadcrumbs, Button} from "@mui/material";
 import {BasicBreadcrumbs} from "@/components/navigation/breadcrumbs";
 import Typography from "@mui/material/Typography";
 import {EventCard} from "@/components/business/EventCard";
 import {Grid} from "@/components/layout/grid";
 import {useEffect, useState} from "react";
 import {type Event } from "@/API/types/types"
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +54,7 @@ export default function Home() {
                   {
                     events.map((event) => (<EventCard key={event.id} event={event}/>))
                   }
+                  <AddEventCard/>
                 </Grid>
               )}
             </div>
@@ -65,4 +68,19 @@ export default function Home() {
       </Container>
     </>
   )
+}
+
+export const AddEventCard = () => {
+
+  return <Card>
+    <CardContent>
+      <div  style={{ display: 'flex', flexDirection: 'column' }}>
+        <Button href={'/event/add'}>
+          <Typography variant={'h1'}>
+            +
+          </Typography>
+        </Button>
+      </div>
+    </CardContent>
+  </Card>
 }
